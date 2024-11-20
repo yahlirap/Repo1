beef8547ea4d9c5bacc6763dfc0409b4713df277
aradharush <aradharush2013@gmail.com>
2024-11-20 17:56:43 +0200

    Arad

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -16,35 +16,35 @@ public class TestThreadCheckArray {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             
-            // Declare threads for the task
+            
             Thread thread1, thread2;
             
-            // Prompt user for ArrayList size
+            
             System.out.println("Enter array size");
             int num = input.nextInt();
             
-            // Initialize the ArrayList based on user input
+            
             ArrayList<Integer> array = new ArrayList<>(num);
             System.out.println("Enter numbers for array");
             
-            // Populate the ArrayList with user input
+            
             for (int index = 0; index < num; index++) 
                 array.add(input.nextInt());
             
-            // Prompt for a number
+           
             System.out.println("Enter number");
             int b = input.nextInt();
             
-            // Initialize SharedData with the ArrayList and the number
+            
             SharedData sd = new SharedData(array, b);
             
-            // Create and start two threads that check the array
+          
             thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
             thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
             thread1.start();
             thread2.start();
             
-            // Wait for both threads to finish
+        
             try {
                 thread1.join();
                 thread2.join();
@@ -52,29 +52,28 @@ public class TestThreadCheckArray {
                 e.printStackTrace();
             }
             
-            // If the flag in SharedData is false, print "Sorry" and exit
             if (!sd.getFlag()) {
                 System.out.println("Sorry");
                 return;
             }
             
-            // Print the solution and results in a formatted manner
+            
             System.out.println("Solution for b : " + sd.getB() + ", n = " + sd.getArray().size());
             System.out.print("I:    ");
             
-            // Print ArrayList indices
+          
             for (int index = 0; index < sd.getArray().size(); index++)
                 System.out.print(index + "    ");
             System.out.println();
             
             System.out.print("A:    ");
             
-            // Print ArrayList elements with formatting based on number length
+   
             for (int index : sd.getArray()) {
                 System.out.print(index);
                 int counter = 5;
                 
-                // Calculate spacing for formatting based on the number's length
+            
                 while (true) {
                     index = index / 10;
                     counter--;
@@ -82,7 +81,7 @@ public class TestThreadCheckArray {
                         break;
                 }
                 
-                // Print spaces for formatting
+         
                 for (int i = 0; i < counter; i++)
                     System.out.print(" ");
             }
@@ -90,7 +89,7 @@ public class TestThreadCheckArray {
             System.out.println();
             System.out.print("C:    ");
             
-            // Print the winArray (boolean values) with formatting
+ 
             for (boolean index : sd.getWinArray()) {
                 if (index)
                     System.out.print("1    ");
